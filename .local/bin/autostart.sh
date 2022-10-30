84e7b6c6c1f8d89bcb6231202313faf9a7f3a7ab
ZaOsJar <imlumxu@gmail.com>
2022-10-30 23:17:46 +0200

    Add more sxhkd keybinds, startx system changed

---
+++ .local/bin/autostart.sh
@@ -0,0 +1,8 @@
+feh --no-fehbg --bg-fill $HOME/Pictures/wallpapers/0016.jpg &
+xrdb -merge $HOME/.Xresources &
+xset r rate 225 32 & # makes the key input more responsive
+xbacklight -set 70 & # laptop specific function
+flameshot &
+sxhkd &
+dunst &
+picom &
